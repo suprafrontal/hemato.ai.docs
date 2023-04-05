0f71bc69e548276c3c04cd6b6a5010c6743f03b3
Ali Moeeny <alimoeeny@gmail.com>
2023-04-05 15:46:53 -0400

    deployed on app runner

---
+++ hematoDocsVersion.go
@@ -0,0 +1,5 @@
+package main
+
+func hematoDocsVersion() string {
+	return "hemato.docs.456.develop.7822295"
+}
---
+++ misc/deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -e
+
+echo this should be run from the root of the project
+
+
+echo Building the static website
+
+# if running for the first time you need to first fetch the image
+# docker build . -t slatedocs/slate
+docker run --rm --name slate -v $(pwd)/build:/srv/slate/build -v $(pwd)/source:/srv/slate/source slatedocs/slate build
+
+
+echo Building the docker image to serve the static website
+
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 460080181115.dkr.ecr.us-east-1.amazonaws.com
+
+export TS=`date +%s`
+
+echo Current Tag is $TS
+
+docker build -t docs.hemato.ai:$TS .
+
+docker tag docs.hemato.ai:$TS 460080181115.dkr.ecr.us-east-1.amazonaws.com/docs.hemato.ai:$TS
+
+docker push 460080181115.dkr.ecr.us-east-1.amazonaws.com/docs.hemato.ai:$TS
